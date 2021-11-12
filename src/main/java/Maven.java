9f00552ca479ba800eed2a809bc1cd41ac5af5e4
Ian McConihay <mcconihay.ian92@gmail.com>
2021-11-12 11:40:47 -0600

    first commit

---
+++ src/main/java/Maven.java
@@ -0,0 +1,18 @@
+import org.apache.commons.lang3.StringUtils;
+import java.util.Scanner;
+public class Maven {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		StringUtils stringUtils = new StringUtils();
+		System.out.println("Enter something: ");
+		scanner.nextLine();
+
+
+
+
+
+
+
+	}
+}
+
