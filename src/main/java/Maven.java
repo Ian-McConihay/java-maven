2637704947bd4a9a2404bb93a21ad3aadada2449
Ian McConihay <mcconihay.ian92@gmail.com>
2021-11-12 11:47:02 -0600

    StringUtils

--- src/main/java/Maven.java
+++ src/main/java/Maven.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 public class Maven {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		StringUtils stringUtils = new StringUtils();
+//		StringUtils stringUtils = new StringUtils();
 		System.out.println("Enter something: ");
-		scanner.nextLine();
+		String input = scanner.next();
 
-
+		System.out.println("Reverse input:" + StringUtils.reverse(input));
+		System.out.println();
+		System.out.println("Your input with flipped case: " + StringUtils.swapCase(input));
+		System.out.println();
+		System.out.println("Is your input a number: " + StringUtils.isNumeric(input));
 
 
 
