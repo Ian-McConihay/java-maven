f73709b5381185797c0844ba1a63e4db9c1cf0bc
Ian McConihay <mcconihay.ian92@gmail.com>
2021-11-12 12:11:11 -0600

    StringUtils cleaned it up a little

--- src/main/java/Maven.java
+++ src/main/java/Maven.java
@@ -2,14 +2,14 @@ import org.apache.commons.lang3.StringUtils;
 import java.util.Scanner;
 public class Maven {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-//		StringUtils stringUtils = new StringUtils();
+		Scanner scanner = new Scanner(System.in).useDelimiter("\n");
 		System.out.println("Enter something: ");
 		String input = scanner.next();
-
 		System.out.println("Reverse input:" + StringUtils.reverse(input) + "\n");
 		System.out.println("Your input with flipped case: " + StringUtils.swapCase(input) + "\n");
-		System.out.println("Is your input a number: " + StringUtils.isNumeric(input));
+		System.out.println("Is your input a number: " + StringUtils.isNumeric(input) + "\n");
+		System.out.println("Input length: " + StringUtils.length(input) + "\n");
+		System.out.println("Contains whitespace: " + StringUtils.containsWhitespace(input) + "\n");
 
 
 
